bac32ea7b77e4aa1b10dc963eff9a4f09b3fa393
elyashedayat10 <98589320+elyashedayat10@users.noreply.github.com>
2022-05-09 17:03:12 +0430

    Update settings.py

--- University/settings.py
+++ University/settings.py
@@ -14,8 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS=['']
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS=['studentschool']
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-$wmy7w146)1tfk*4k8o$&1a$#0i-y_m@rjj!v_(38k-18jm@y9'
 # SECRET_KEY = os.getenv("SECRET_KEY")
