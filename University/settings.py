fbec7894b39f0531cdf3f70f9771ff017283140e
elyashedayat10 <98589320+elyashedayat10@users.noreply.github.com>
2022-05-09 17:52:49 +0430

    Update settings.py

--- University/settings.py
+++ University/settings.py
@@ -35,8 +35,8 @@ INSTALLED_APPS = [
     "django_jalali",
     "django_cleanup",
     "widget_tweaks",
-    "silk",
-    "debug_toolbar",
+#     "silk",
+#     "debug_toolbar",
     "django_filters",
 
     # local
@@ -153,18 +153,18 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 #     },
 # }
 AUTH_USER_MODEL = "account.User"
-INTERNAL_IPS = ('127.0.0.1', '0.0.0.0', 'localhost',)
-DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-]
+# INTERNAL_IPS = ('127.0.0.1', '0.0.0.0', 'localhost',)
+# DEBUG_TOOLBAR_PANELS = [
+#     'debug_toolbar.panels.versions.VersionsPanel',
+#     'debug_toolbar.panels.timer.TimerPanel',
+#     'debug_toolbar.panels.settings.SettingsPanel',
+#     'debug_toolbar.panels.headers.HeadersPanel',
+#     'debug_toolbar.panels.request.RequestPanel',
+#     'debug_toolbar.panels.sql.SQLPanel',
+#     'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+#     'debug_toolbar.panels.templates.TemplatesPanel',
+#     'debug_toolbar.panels.cache.CachePanel',
+#     'debug_toolbar.panels.signals.SignalsPanel',
+#     'debug_toolbar.panels.logging.LoggingPanel',
+#     'debug_toolbar.panels.redirects.RedirectsPanel',
+# ]
